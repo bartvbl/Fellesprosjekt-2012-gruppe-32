32ffb02fcc4730dd3d07c4f336e456267d26d823
Bart van Blokland <bartvblokl@gmail.com>
2012-03-19 13:39:47 +0100

    the main window now resizes properly

--- Fellesprosjekt/src/fp/componentControllers/CalendarViewResizeController.java
+++ Fellesprosjekt/src/fp/componentControllers/CalendarViewResizeController.java
@@ -11,7 +11,7 @@ import fp.views.CalendarView;
 public class CalendarViewResizeController extends AbstractComponentController implements EventHandler {
 
 	private JFrame mainWindowFrame;
-	private static final int vertical_offset = -50;
+	private static final int vertical_offset = -60;
 	private static final int horizontal_offset = -20;
 
 	public CalendarViewResizeController(EventDispatcher eventDispatcher) {
@@ -28,8 +28,8 @@ public class CalendarViewResizeController extends AbstractComponentController im
 		if(event.eventType == EventType.WINDOW_RESIZED) {
 			int width = this.mainWindowFrame.getWidth();
 			int height = this.mainWindowFrame.getHeight();
-			System.out.println("updating window size: " + width + ", " + height);
-			CalendarView.mainPanel.setBounds(0, 0, width + horizontal_offset, height + vertical_offset);
+			CalendarView.mainPanel.setBounds(5, 5, width + horizontal_offset, height + vertical_offset);
+			this.mainWindowFrame.validate();
 		}
 	}
 
