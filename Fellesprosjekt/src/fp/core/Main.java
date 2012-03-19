1c7f8c294e3607acf60d85f6887dd95febf3499e
Bart van Blokland <bartvblokl@gmail.com>
2012-03-19 11:45:29 +0100

    added updated views

--- Fellesprosjekt/src/fp/core/Main.java
+++ Fellesprosjekt/src/fp/core/Main.java
@@ -1,19 +1,7 @@
 package fp.core;
 
-import java.util.ArrayList;
-
-import no.ntnu.fp.net.cl.ClSocket;
-import no.ntnu.fp.net.cl.KtnDatagram;
-
 public class Main {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		System.out.println("nothing here :(");
-//		ClSocket socket = new ClSocket();
-//		KtnDatagram datagram = new KtnDatagram();
+		
 	}
-
 }
