c37e2e51f2bc9dd9efbb62a9ea41facd5bf4bdee
Hassel <Hassel@78.91.21.71>
2012-03-20 13:24:54 +0100

    GetMeetingsInWeek

---
+++ Fellesprosjekt/src/fp/messageHandlers/GetMeetingsInWeekHandler.java
@@ -0,0 +1,47 @@
+package fp.messageHandlers;
+
+import java.sql.SQLException;
+
+import nu.xom.Element;
+
+import fp.dataObjects.Meeting;
+import fp.dataObjects.Meeting.LocationType;
+import fp.dataObjects.Meeting.Status;
+import fp.database.DatabaseConnection;
+import fp.messageParsers.Message;
+import fp.xmlConverters.MeetingConverter;
+
+public class GetMeetingsInWeekHandler implements MessageHandler {
+
+	@Override
+	public void handleMessage(Message message) throws SQLException {
+		Meeting meeting = MeetingConverter.convertXMLToMeeting(message.getData());
+		// skal hente møte basert på starttime og endtime
+		
+		String[] fromDateToDate = extractDatesFromMessageData(message).split("+");
+		String fromDate = fromDateToDate[0];
+		String toDate = fromDateToDate[1];
+		
+		String sqlQurey = "SELECT Meeting FROM MEETING WHERE" + fromDate + "" + toDate + ");";
+		
+		DatabaseConnection.executeWriteQuery(sqlQurey);		
+	}
+	
+	public String extractDatesFromMessageData(Message message){
+		
+		
+		String fromDate = null, toDate = null;
+		Element element = message.getData().getFirstChildElement("fromDate");
+		if (element != null) {
+			fromDate = element.getValue();
+		}
+		element = message.getData().getFirstChildElement("locationType");
+		if (element != null) {
+			toDate = element.getValue();
+					
+		}
+		
+		return fromDate + "+" + toDate;
+	}
+
+}
