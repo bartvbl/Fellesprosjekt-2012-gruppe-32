4ef716b73b64122f828a769d1fce1f5d3fb07421
Anders Boe <anders.boe@hotmail.com>
2012-03-20 12:44:10 +0100

    Add message handler, messageHandler har fått en throw SQLException

--- Fellesprosjekt/src/fp/messageHandlers/AddMeetingHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/AddMeetingHandler.java
@@ -1,16 +1,23 @@
 package fp.messageHandlers;
 
+import java.sql.SQLException;
+
 import fp.dataObjects.Meeting;
 import fp.database.DatabaseConnection;
 import fp.messageParsers.Message;
+import fp.xmlConverters.MeetingConverter;
 
 public class AddMeetingHandler implements MessageHandler {
 
-	private Meeting meeting;
-	
+
 	@Override
-	public void handleMessage(Message message) {
-		DatabaseConnection.connect();
+	public void handleMessage(Message message) throws SQLException {
+		Meeting meeting = MeetingConverter.convertXMLToMeeting(message.getData());
+		
+		String sqlQurey = "INSERT INTO Meeting VALUES(NULL, " + meeting.description + ", " + meeting.location + ", " + meeting.locationType + ", " 
+						+ meeting.startTime + ", " + meeting.endTime + ", " + meeting.status + ");";
+		
+		DatabaseConnection.executeWriteQuery(sqlQurey);
 		
 	}
 	
--- Fellesprosjekt/src/fp/messageHandlers/MessageHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/MessageHandler.java
@@ -1,10 +1,12 @@
 package fp.messageHandlers;
 
+import java.sql.SQLException;
+
 import fp.messageParsers.Message;
 
 public interface MessageHandler {
 	
-	public void handleMessage(Message message);
+	public void handleMessage(Message message) throws SQLException;
 		
 }
 
