73077147d0c950bcb6edbcdd1530af6c2424f48a
Anders Boe <anders.boe@hotmail.com>
2012-03-20 11:46:33 +0100

    MessageHandlers

---
+++ Fellesprosjekt/src/fp/messageHandlers/GetUserHandler.java
@@ -0,0 +1,13 @@
+package fp.messageHandlers;
+
+import fp.messageParsers.Message;
+
+public class GetUserHandler implements MessageHandler {
+
+	@Override
+	public void handleMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
