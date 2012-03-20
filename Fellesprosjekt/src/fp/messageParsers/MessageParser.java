db0e1186e1c2d010bcca383edda4a72971cffc78
Hassel <Hassel@78.91.21.71>
2012-03-20 11:44:39 +0100

    Lagt til hashmap i MessageParser

--- Fellesprosjekt/src/fp/messageParsers/MessageParser.java
+++ Fellesprosjekt/src/fp/messageParsers/MessageParser.java
@@ -1,9 +1,31 @@
 package fp.messageParsers;
 
+import java.util.HashMap;
+
+import fp.messageHandlers.AddMeetingHandler;
+import fp.messageHandlers.GetMeetingHandler;
+import fp.messageHandlers.MessageHandler;
+
 public class MessageParser {
 	
+	private static HashMap<MessageType, MessageHandler> typeForHandlerMap = new HashMap<MessageType, MessageHandler>();
 	
-	public void parseMessage(){
+	public MessageParser(){
+		typeForHandlerMap.put(MessageType.addMeeting, new AddMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		
+	}
+	
+	public static void parseMessage(Message message){
+
+		
 		
 	}
 	
