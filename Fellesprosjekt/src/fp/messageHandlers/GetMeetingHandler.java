957fd96eb8738d4af799d3d7db812392827265b5
Anette Ellingsberg <anetteellings@gmail.com>
2012-03-20 13:09:03 +0100

    GetMeeting

--- Fellesprosjekt/src/fp/messageHandlers/GetMeetingHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/GetMeetingHandler.java
@@ -1,14 +1,23 @@
 
 package fp.messageHandlers;
 
+import java.sql.SQLException;
+
+import fp.dataObjects.Meeting;
+import fp.database.DatabaseConnection;
 import fp.messageParsers.Message;
+import fp.xmlConverters.MeetingConverter;
 
 public class GetMeetingHandler implements MessageHandler{
 
 	@Override
-	public void handleMessage(Message message) {
-		// TODO Auto-generated method stub
+	public void handleMessage(Message message) throws SQLException {
+		Meeting meeting = MeetingConverter.convertXMLToMeeting(message.getData());
+		// skal hente møte basert på meetingID
+		String sqlQurey = "SELECT Meeting FROM MEETING WHERE" + meeting.meetingID + ");";
 		
+		DatabaseConnection.executeWriteQuery(sqlQurey);
+
 	}
 
 }
