e3b08ef68c1cc7d7b89e9244054ab2d887d1b294
Hassel <Hassel@78.91.21.71>
2012-03-20 11:57:33 +0100

    blah

--- Fellesprosjekt/src/fp/messageParsers/MessageParser.java
+++ Fellesprosjekt/src/fp/messageParsers/MessageParser.java
@@ -2,23 +2,33 @@ package fp.messageParsers;
 
 import java.util.HashMap;
 
+import fp.messageHandlers.AddFavouriteHandler;
+import fp.messageHandlers.AddMeetingHandler;
 import fp.messageHandlers.GetMeetingHandler;
+import fp.messageHandlers.GetUserHandler;
 import fp.messageHandlers.MessageHandler;
+import fp.messageHandlers.UpdateMeetingHandler;
 
 public class MessageParser {
 	
 	private static HashMap<MessageType, MessageHandler> typeForHandlerMap = new HashMap<MessageType, MessageHandler>();
 	
+	/*
+	addMeeting, 
+	removeMeeting, 
+	updateMeeting, 
+	getMeeting, 
+	getUser, 
+	addFavourite
+	*/
+	
 	public MessageParser(){
 		typeForHandlerMap.put(MessageType.addMeeting, new AddMeetingHandler());
 		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.updateMeeting, new UpdateMeetingHandler());
 		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
-		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
-		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
-		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
-		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
-		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
-		typeForHandlerMap.put(MessageType.getMeeting, new GetMeetingHandler());
+		typeForHandlerMap.put(MessageType.getUser, new GetUserHandler());
+		typeForHandlerMap.put(MessageType.addFavourite, new AddFavouriteHandler());
 		
 	}
 	
