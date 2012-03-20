c6fc2a3fe21193890e912bae3e2bf42ed33b5d9e
Anders Boe <anders.boe@hotmail.com>
2012-03-20 11:52:37 +0100

    Message handlers

--- Fellesprosjekt/src/fp/messageHandlers/GetMeetingHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/GetMeetingHandler.java
@@ -3,20 +3,6 @@ package fp.messageHandlers;
 
 import fp.messageParsers.Message;
 
-public class GetMeetingHandler implements MessageHandler {
-
-	@Override
-	public void handleMessage(Message message) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-=======
-package fp.messageHandlers;
-
-import fp.messageParsers.Message;
-
 public class GetMeetingHandler implements MessageHandler{
 
 	@Override
--- Fellesprosjekt/src/fp/messageHandlers/GetUserHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/GetUserHandler.java
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-package fp.messageHandlers;
 
-import fp.messageParsers.Message;
-
-public class GetUserHandler implements MessageHandler {
-
-	@Override
-	public void handleMessage(Message message) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
-=======
 package fp.messageHandlers;
 
 import fp.messageParsers.Message;
@@ -26,4 +12,3 @@ public class GetUserHandler implements MessageHandler{
 	}
 
 }
->>>>>>> branch 'master' of https://andersboe@github.com/bartvbl/Fellesprosjekt-2012-gruppe-32.git
