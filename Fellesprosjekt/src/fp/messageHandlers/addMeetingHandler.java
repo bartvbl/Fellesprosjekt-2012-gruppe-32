11678e9c89ec83d5a0714cff98edc3989145b482
Simen Støa <simen.stoa@gmail.com>
2012-03-20 11:48:38 +0100

    startet på AddMeetingHandler

--- Fellesprosjekt/src/fp/messageHandlers/addMeetingHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/addMeetingHandler.java
@@ -1,13 +1,22 @@
 package fp.messageHandlers;
 
+import fp.dataObjects.Meeting;
+import fp.database.DatabaseConnection;
 import fp.messageParsers.Message;
 
 public class addMeetingHandler implements MessageHandler {
 
+	private Meeting meeting;
+	
 	@Override
 	public void handleMessage(Message message) {
-		// TODO Auto-generated method stub
-
+		DatabaseConnection.connect();
+		
 	}
+	
+	
+	
+
+	
 
 }
