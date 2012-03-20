cd567f1e5b676310357926feafbfbfdd7068ee87
Anette Ellingsberg <anetteellings@gmail.com>
2012-03-20 14:50:54 +0100

    AddFavouriteUserHandler

--- Fellesprosjekt/src/fp/messageHandlers/AddFavouriteUserHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/AddFavouriteUserHandler.java
@@ -2,15 +2,24 @@ package fp.messageHandlers;
 
 import java.sql.SQLException;
 
+import fp.dataObjects.Meeting;
 import fp.dataObjects.ServerUserData;
+import fp.dataObjects.User;
+import fp.database.DatabaseConnection;
 import fp.messageParsers.Message;
+import fp.xmlConverters.MeetingConverter;
+import fp.xmlConverters.UserConverter;
 
 public class AddFavouriteUserHandler implements MessageHandler {
 
 	@Override
-	public void handleMessage(Message message, ServerUserData userdata) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void handleMessage(Message message, ServerUserData userData) throws SQLException {
+		
+		User user = UserConverter.convertXMLToUser(message.getData());
+		
+		String sqlQurey = "INSERT INTO UserFavourites VALUES("+ userData.getUser().userID+ " , 'user', " + user.userID + ", NULL);";
+		
+		DatabaseConnection.executeWriteQuery(sqlQurey);
 	}
 
 }
