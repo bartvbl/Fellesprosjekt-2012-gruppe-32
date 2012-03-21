81fffd06f5e0b042689a7587fa63155d253b0c98
Anders Boe <anders.boe@hotmail.com>
2012-03-21 14:03:08 +0100

    update

--- Fellesprosjekt/src/fp/messageHandlers/AddMeetingHandler.java
+++ Fellesprosjekt/src/fp/messageHandlers/AddMeetingHandler.java
@@ -13,14 +13,20 @@ public class AddMeetingHandler implements MessageHandler {
 
 	@Override
 	public void handleMessage(Message message, ServerUserData userdata) throws SQLException {
+		System.out.println("Starter konvertering...");
 		Meeting meeting = MeetingConverter.convertXMLToMeeting(message.getData());
+		
+		System.out.println("Konvertert til XML fil...");
+		System.out.println("Oppretter SQL streng");
 		
 		String sqlQurey = "INSERT INTO Meeting VALUES(null, " 	+ meeting.description + ", " + meeting.status + "," + meeting.location + ", " 
 																+ meeting.locationType + ", " + meeting.roomID + ", " + meeting.startTime + ", " 
 																+ meeting.endTime + ", " + meeting.creatorID + "," + meeting.meetingType + ");";
 		
+		System.out.println("Setter inn i database");
 		DatabaseConnection.executeWriteQuery(sqlQurey);
 		
+		System.out.println("Ferdig");
 	}
 	
 
