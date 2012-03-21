9f429ac6c75e4f8e876f0f6218733604b9c8edcb
Simen Støa <simen.stoa@gmail.com>
2012-03-21 15:57:34 +0100

    lagt til flere listeners

--- Fellesprosjekt/src/fp/components/newMeeting/NewMeetingHandler.java
+++ Fellesprosjekt/src/fp/components/newMeeting/NewMeetingHandler.java
@@ -2,17 +2,22 @@ package fp.components.newMeeting;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import fp.componentHandlers.AbstractComponentHandler;
 import fp.componentHandlers.ComponentHandlerType;
+import fp.events.Event;
 import fp.events.EventDispatcher;
+import fp.events.EventType;
 import fp.views.NewMeetingWindow;
 import fp.views.SmallCalendarPanel;
 
-public class NewMeetingHandler extends AbstractComponentHandler implements ActionListener, ListSelectionListener{
+public class NewMeetingHandler extends AbstractComponentHandler implements ActionListener, ListSelectionListener, KeyListener{
 	
 	NewMeetingController controller;
 
@@ -26,7 +31,7 @@ public class NewMeetingHandler extends AbstractComponentHandler implements Actio
 	public void actionPerformed(ActionEvent event) {
 		if(event.getSource() == NewMeetingWindow.createButton) {
 			this.controller.create();
-		} else if(event.getSource() == SmallCalendarPanel.prevMonthButton) {
+		} else if(event.getSource() == NewMeetingWindow.cancelButton) {
 			this.controller.cancel();
 		}
 	}
@@ -38,10 +43,39 @@ public class NewMeetingHandler extends AbstractComponentHandler implements Actio
 		NewMeetingWindow.newMeetingButton.addActionListener(this);
 		NewMeetingWindow.meetingRoomSearchResultList.getSelectionModel().addListSelectionListener(this);
 		NewMeetingWindow.participantSearchResultList.getSelectionModel().addListSelectionListener(this);
+		NewMeetingWindow.startDateTextPane.addKeyListener(this);
+		NewMeetingWindow.startTimeTextPane.addKeyListener(this);
+		NewMeetingWindow.endDateTextPane.addKeyListener(this);
+		NewMeetingWindow.endDateTextPane.addKeyListener(this);
+		NewMeetingWindow.appointmentTitleTextPane.addKeyListener(this);
+		NewMeetingWindow.locationSearchTextPane.addKeyListener(this);
+		NewMeetingWindow.manualLocationTextPane.addKeyListener(this);
+		NewMeetingWindow.participantSearchTextPane.addKeyListener(this);
 	}
 
 	@Override
-	public void valueChanged(ListSelectionEvent e) {
+	public void valueChanged(ListSelectionEvent event) {
+		DefaultListSelectionModel model = (DefaultListSelectionModel)event.getSource();
+		if(model.getLeadSelectionIndex() != -1) {
+			
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getSource()==NewMeetingWindow.appointmentTitleTextPane){
+			
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
