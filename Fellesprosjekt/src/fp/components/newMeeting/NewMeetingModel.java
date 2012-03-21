6d584664c46c249b1a44ae2f74724e1290a3d208
Simen Støa <simen.stoa@gmail.com>
2012-03-21 15:04:23 +0100

    laget gettere/settere i NewMeetingModel

--- Fellesprosjekt/src/fp/components/newMeeting/NewMeetingModel.java
+++ Fellesprosjekt/src/fp/components/newMeeting/NewMeetingModel.java
@@ -1,8 +1,88 @@
 package fp.components.newMeeting;
 
 import fp.dataObjects.Meeting;
+import fp.dataObjects.Meeting.LocationType;
+import fp.dataObjects.Meeting.MeetingType;
+import fp.dataObjects.Meeting.Status;
 
 public class NewMeetingModel {
 	
 	private Meeting meeting;
+	
+	private String description = null;
+	private String location = null;
+	private LocationType locationType = null;
+	private String startTime = null;
+	private String endTime = null;
+	private Status status = null;
+	private int creatorID = 0;
+	private int roomID = 0;
+	private MeetingType meetingtype;
+	
+	public void createMeeting(){
+		meeting = new Meeting(0, description, location, locationType, startTime, endTime, status, creatorID, roomID, meetingtype);
+	}
+	
+	public Meeting getMeeting() {
+		return meeting;
+	}
+	public void setMeeting(Meeting meeting) {
+		this.meeting = meeting;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public LocationType getLocationType() {
+		return locationType;
+	}
+	public void setLocationType(LocationType locationType) {
+		this.locationType = locationType;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public int getCreatorID() {
+		return creatorID;
+	}
+	public void setCreatorID(int creatorID) {
+		this.creatorID = creatorID;
+	}
+	public int getRoomID() {
+		return roomID;
+	}
+	public void setRoomID(int roomID) {
+		this.roomID = roomID;
+	}
+	public MeetingType getMeetingtype() {
+		return meetingtype;
+	}
+	public void setMeetingtype(MeetingType meetingtype) {
+		this.meetingtype = meetingtype;
+	}
+	
+	
 }
