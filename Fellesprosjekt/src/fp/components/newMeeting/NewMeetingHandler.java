897e0fd7b3d92cf41ca3a5361d7dce4d2450a338
Simen Støa <simen.stoa@gmail.com>
2012-03-21 15:20:41 +0100

    lagt til noen listeners

--- Fellesprosjekt/src/fp/components/newMeeting/NewMeetingHandler.java
+++ Fellesprosjekt/src/fp/components/newMeeting/NewMeetingHandler.java
@@ -3,13 +3,16 @@ package fp.components.newMeeting;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import fp.componentHandlers.AbstractComponentHandler;
 import fp.componentHandlers.ComponentHandlerType;
 import fp.events.EventDispatcher;
 import fp.views.NewMeetingWindow;
 import fp.views.SmallCalendarPanel;
 
-public class NewMeetingHandler extends AbstractComponentHandler implements ActionListener{
+public class NewMeetingHandler extends AbstractComponentHandler implements ActionListener, ListSelectionListener{
 	
 	NewMeetingController controller;
 
@@ -33,6 +36,14 @@ public class NewMeetingHandler extends AbstractComponentHandler implements Actio
 		NewMeetingWindow.cancelButton.addActionListener(this);
 		NewMeetingWindow.newAppointmentButton.addActionListener(this);
 		NewMeetingWindow.newMeetingButton.addActionListener(this);
+		NewMeetingWindow.meetingRoomSearchResultList.getSelectionModel().addListSelectionListener(this);
+		NewMeetingWindow.participantSearchResultList.getSelectionModel().addListSelectionListener(this);
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
