da6b6ebbee3bc0d754145faa91f3372b90845344
Bart van Blokland <bartiver@stud.ntnu.no>
2012-03-26 16:55:41 +0200

    improved some conversion code

--- Fellesprosjekt/src/fp/xmlConverters/NotificationConverter.java
+++ Fellesprosjekt/src/fp/xmlConverters/NotificationConverter.java
@@ -54,10 +54,9 @@ public Notification toNotification(String xml) throws java.io.IOException, java.
 public static Notification convertXMLToNotification(Element notificationElement) {
 	
 	String userID = null, meetingID = null;
-	NotificationStatus aM = null;
-	NotificationType nT = null;
+	NotificationStatus notificationStatus = null;
+	NotificationType nofiticationType = null;
 	
-
 	Element element = notificationElement.getFirstChildElement("userID");
 	if (element != null) {
 		userID = element.getValue();
@@ -68,25 +67,14 @@ public static Notification convertXMLToNotification(Element notificationElement)
 	}
 	element = notificationElement.getFirstChildElement("acceptedMeeting");
 	if (element != null) {
-		if(element.getValue().equals("Yes")){
-			aM = aM.Yes;
-		}else if(element.getValue().equals("No")){
-			nT = nT.newMeeting;
-		}
+		notificationStatus = Enum.valueOf(NotificationStatus.class, element.getValue());
 	}
 	element = notificationElement.getFirstChildElement("notificationType");
 	if (element != null) {
-		if(element.getValue().equals("newMeeting")){
-			nT = nT.newMeeting;
-		}else if(element.getValue().equals("meetingUpdated")){
-			nT = nT.meetingUpdated;
-		}else if(element.getValue().equals("meetingCancelled")){
-			nT = nT.meetingCancelled;
-		}
+		nofiticationType = Enum.valueOf(NotificationType.class, element.getValue());
 	}
 	
-	return new Notification(Integer.parseInt(userID), Integer.parseInt(meetingID), aM, nT);
-	
+	return new Notification(Integer.parseInt(userID), Integer.parseInt(meetingID), notificationStatus, nofiticationType);
 }
 
 }
