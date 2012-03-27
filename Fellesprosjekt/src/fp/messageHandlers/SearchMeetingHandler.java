d8eb246a50e79f2f5a419a67de90f02b9c9e3bab
Hassel <Hassel@78.91.21.129>
2012-03-27 11:11:26 +0200

    To nye handlers

---
+++ Fellesprosjekt/src/fp/messageHandlers/SearchMeetingHandler.java
@@ -0,0 +1,17 @@
+package fp.messageHandlers;
+
+import java.sql.SQLException;
+
+import fp.messageParsers.Message;
+import fp.server.ServerClientContext;
+
+public class SearchMeetingHandler implements MessageHandler{
+
+	@Override
+	public void handleMessage(Message message, ServerClientContext clientContext)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
