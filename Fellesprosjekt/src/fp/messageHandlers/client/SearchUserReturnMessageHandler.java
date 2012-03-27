d81f9d0afcdc04608e15fa8c08b0c4471ad74ca9
Hassel <Hassel@78.91.21.129>
2012-03-27 13:47:24 +0200

    lagt til ny pakke fp.messageHandlers.client

---
+++ Fellesprosjekt/src/fp/messageHandlers/client/SearchUserReturnMessageHandler.java
@@ -0,0 +1,15 @@
+package fp.messageHandlers.client;
+
+import fp.messageParsers.Message;
+import fp.messageParsers.client.ClientMessageHandler;
+import fp.net.client.ClientConnectionContext;
+
+public class SearchUserReturnMessageHandler implements ClientMessageHandler{
+
+	@Override
+	public void handleMessage(Message message, ClientConnectionContext context) {
+		
+		
+	}
+
+}
